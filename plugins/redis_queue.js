088dbf7edd2a08abdfd0e090843f6e010bbfcc5c
Bruno Wego <brunowego@gmail.com>
2020-01-08 12:22:24 -0300

    chore(npm): change redis package

--- plugins/redis_queue.js
+++ plugins/redis_queue.js
@@ -1,4 +1,4 @@
-var redis = require('node-redis'),
+var redis = require('redis'),
   credentials = require('../lib/bmservice').getCredentials(/^Redis by Compose/) || { username: "", password: "", public_hostname: "localhost/6379"};
   bits = credentials.public_hostname.split('/'),
   hostname = bits[0],
